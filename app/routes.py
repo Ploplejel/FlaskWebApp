e080d2ae23c211b51aa2ddd06a1be5fac04d17e9
Antoni Goles <vezfx@wp.pl>
2018-12-04 17:33:40 +0100

    flask #3 publik

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,12 @@ def index():
     return render_template('index.html')
 
 @app.route('/galeria')
-def getGaleria():
-    obrazki = ["http://memy.pl/show/big/uploads/Post/72312/14677121051422.jpg", "https://www.wprost.pl/_thumb/d0/90/d0dd798bc6dcfd022a8eef89963a.jpeg", "https://pobierak.jeja.pl/images/4/b/a/248059_wybierz-klase-postaci.jpg"]
-    return render_template('galeria.html')
+def getGalery():
+    listaObrazkow = [
+        'https://media.giphy.com/media/1373LnkaxqwGKQ/giphy.gif',
+        'https://upload.wikimedia.org/wikipedia/commons/2/2c/Rotating_earth_%28large%29.gif',
+        'http://cdn.benchmark.pl/uploads/products/5874/intel-core-i7-7700k.jpg',
+        'http://arcadiawindber.com/wp-content/uploads/2017/12/ho-ho-ho-clipart.jpg',
+        'https://cdn2.unrealengine.com/Fortnite%2Fbattle-royale%2Fseason6-social-1920x1080-0a72ec2f35dfe5be6cf8a77ec16063cca4db7046.jpg'
+    ]
+    return render_template('galeria.html', listaObrazkow=listaObrazkow, title="Galeria")
